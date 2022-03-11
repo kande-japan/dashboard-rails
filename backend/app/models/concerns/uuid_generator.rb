c6a0f3233a8e2bdfc750c59a6869899bc20973bf
Hiroaki Honda <82698421+Damin3927@users.noreply.github.com>
2022-03-11 11:26:23 +0900

    Change uuid_generator to use Concern

--- backend/app/models/concerns/uuid_generator.rb
+++ backend/app/models/concerns/uuid_generator.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-module UUIDGenerator
-  def self.included(klass)
-    klass.before_create :generate_uuid
+module UuidGenerator
+  extend ActiveSupport::Concern
+
+  included do
+    before_create :generate_uuid
   end
 
   # Generate UUID record in model with name "id"
   def generate_uuid
     self.id = loop do
       random_id = SecureRandom.uuid
-      break random_id if self.class.empty?(id: random_id)
+      break random_id unless self.class.exists?(id: random_id)
     end
   end
 end
